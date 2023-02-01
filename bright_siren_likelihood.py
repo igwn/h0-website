568c8b896acb157ac870793b0108d8c9d7481eb2
Luis Miguel Galvis <luismiguel.galvis@ligo.org>
2023-02-01 01:14:30 -0500

    Graphics with electromagnetic counterpart

--- bright_siren_likelihood.py
+++ bright_siren_likelihood.py
@@ -75,53 +75,60 @@ class H0likelihood :
         self.bright_siren_dictionary = {}
         
         for event in bright_siren_information_dictionary.keys () :
-            
-            skymap = bright_siren_information_dictionary [event] ["skymap"]
-            counterpart_ra = bright_siren_information_dictionary [event] ["counterpart_ra"]
-            counterpart_dec = bright_siren_information_dictionary [event] ["counterpart_dec"]
-            
-            # skymap
-            (prob, distmu, distsigma, distnorm), metadata = read_sky_map (skymap, distances=True, moc=False, nest=True)
-            
-            # pixel corresponding to sky position of identified galaxy	
-            npix = len(prob)
-            nside = hp.npix2nside (npix)
-            counterpart_pix = hp.ang2pix (nside, np.pi/2 - counterpart_dec, counterpart_ra, nest=True)
-            
-            # minimum and maximum distance of GW event
-            mu = distmu[(distmu < np.inf) & (distmu > 0)]
-            self.dlGWmin = 0.5*min(mu)
-            self.dlGWmax = 2*max(mu)
-            
-            # counterpart information
-            counterpart_muz = bright_siren_information_dictionary [event] ["counterpart_cz"]/cval
-            counterpart_sigmaz =  bright_siren_information_dictionary [event] ["counterpart_sigma_cz"]/cval
-            a = (0.0 - counterpart_muz) / counterpart_sigmaz
-            counterpart_z_array = np.linspace (0.5*counterpart_muz, 2*counterpart_muz, redshift_bins)
-            counterpart_pdf = truncnorm (a,5, counterpart_muz, counterpart_sigmaz).pdf (counterpart_z_array)
-        
-            # redshift prior
-            dVc_by_dz = diff_comoving_vol (counterpart_z_array)
-            pz = self.model (counterpart_z_array)*dVc_by_dz/(1+counterpart_z_array)
-        
-            # likelihood in luminosity distance from skymap
-            distmu_los = distmu [counterpart_pix]
-            distsigma_los = distsigma [counterpart_pix]
-            likelihood_x_dl_skymap = norm (distmu_los, distsigma_los) 
-            
-            
+
             self.bright_siren_dictionary [event] = {}
-            self.bright_siren_dictionary [event] ["counterpart_z_array"] = counterpart_z_array
-            self.bright_siren_dictionary [event] ["counterpart_pdf"] = counterpart_pdf
-            self.bright_siren_dictionary [event] ["likelihood"] = likelihood_x_dl_skymap
-            self.bright_siren_dictionary [event] ["z_prior"] = pz
+
+            for em_info in bright_siren_information_dictionary [event]['Counterparts'].keys() :
+
+                em_name = bright_siren_information_dictionary[event]["Counterparts"][em_info]["Name"]
+            
+                skymap = bright_siren_information_dictionary [event] ["Skymap"]
+                counterpart_ra = bright_siren_information_dictionary [event] ['Counterparts'] [em_info] ["Parameters"] ["counterpart_ra"]
+                counterpart_dec = bright_siren_information_dictionary [event] ['Counterparts'] [em_info] ["Parameters"] ["counterpart_dec"]
+            
+                # skymap
+                (prob, distmu, distsigma, distnorm), metadata = read_sky_map (skymap, distances=True, moc=False, nest=True)
+            
+                # pixel corresponding to sky position of identified galaxy	
+                npix = len(prob)
+                nside = hp.npix2nside (npix)
+                counterpart_pix = hp.ang2pix (nside, np.pi/2 - counterpart_dec, counterpart_ra, nest=True)
+            
+                # counterpart information
+                counterpart_muz = bright_siren_information_dictionary [event] ['Counterparts'] [em_info] ["Parameters"] ["counterpart_cz"]/cval
+                counterpart_sigmaz =  bright_siren_information_dictionary [event] ['Counterparts'] [em_info] ["Parameters"] ["counterpart_sigma_cz"]/cval
+                a = (0.0 - counterpart_muz) / counterpart_sigmaz
+                counterpart_z_array = np.linspace (0.5*counterpart_muz, 2*counterpart_muz, redshift_bins)
+                counterpart_pdf = truncnorm (a,5, counterpart_muz, counterpart_sigmaz).pdf (counterpart_z_array)
+        
+                # redshift prior
+                dVc_by_dz = diff_comoving_vol (counterpart_z_array)
+                pz = self.model (counterpart_z_array)*dVc_by_dz/(1+counterpart_z_array)
+        
+                # likelihood in luminosity distance from skymap
+                distmu_los = distmu [counterpart_pix]
+                distsigma_los = distsigma [counterpart_pix]
+                likelihood_x_dl_skymap = norm (distmu_los, distsigma_los) 
+
+                # minimum and maximum distance of GW event
+                self.dlGWmin = distmu_los - 5*distsigma_los
+                self.dlGWmax = distmu_los + 5*distsigma_los
+                if self.dlGWmin <=0 :
+                    self.dlGWmin = 0
+            
+            
+                self.bright_siren_dictionary [event] [em_name] = {}
+                self.bright_siren_dictionary [event] [em_name] ["counterpart_z_array"] = counterpart_z_array
+                self.bright_siren_dictionary [event] [em_name] ["counterpart_pdf"] = counterpart_pdf
+                self.bright_siren_dictionary [event] [em_name] ["likelihood"] = likelihood_x_dl_skymap
+                self.bright_siren_dictionary [event] [em_name] ["z_prior"] = pz
 
         # save likelihood information
         likelihood_df = self.H0likelihood_events ()
         likelihood_df.insert (0, "H0", self.H0_array) 
         likelihood_df.to_csv (filename, index=False)
     
-    def likelihood_x_z_H0_single_event (self, event, H0, counterpart_z_array, redshift_bins_temp = 10000) :
+    def likelihood_x_z_H0_single_event (self, event, H0, em_name, counterpart_z_array, redshift_bins_temp = 10000) :
     
         cosmo = FlatLambdaCDM(H0=H0, Om0=self.Om0, Tcmb0=2.725)
         zmin = self.dlH02z(self.dlGWmin*H0) *0.5
@@ -130,7 +137,7 @@ class H0likelihood :
         zGW_array_temp = np.linspace (zmin,zmax,redshift_bins_temp)
         dl_array_temp = cosmo.luminosity_distance (zGW_array_temp).value
         
-        likelihood_x_z_H0= self.bright_siren_dictionary [event] ["likelihood"].pdf(dl_array_temp)
+        likelihood_x_z_H0= self.bright_siren_dictionary [event] [em_name] ["likelihood"].pdf(dl_array_temp)
         likelihood_x_z_H0 /= simpson (likelihood_x_z_H0, zGW_array_temp)
         
         px_z_H0_interp = interp1d(zGW_array_temp,likelihood_x_z_H0,kind="linear",bounds_error=False,fill_value=0)
@@ -143,23 +150,24 @@ class H0likelihood :
         likelihood_dictionary = {}
 
         for event in self.bright_siren_dictionary.keys () :
+            for em_name in self.bright_siren_dictionary[event].keys() :
 
-            counterpart_z_array_event = self.bright_siren_dictionary [event] ["counterpart_z_array"]
-            zprior_event = self.bright_siren_dictionary [event] ["z_prior"]
-            counterpart_pdf = self.bright_siren_dictionary [event] ["counterpart_pdf"]
+                counterpart_z_array_event = self.bright_siren_dictionary [event]  [em_name] ["counterpart_z_array"]
+                zprior_event = self.bright_siren_dictionary [event]  [em_name] ["z_prior"]
+                counterpart_pdf = self.bright_siren_dictionary [event]  [em_name] ["counterpart_pdf"]
 
-            likelihood_array = np.zeros (self.H0bins)
+                likelihood_array = np.zeros (self.H0bins)
 
-            for hh, H0 in enumerate (self.H0_array) :
+                for hh, H0 in enumerate (self.H0_array) :
 
-                px_z_H0 = self.likelihood_x_z_H0_single_event (event, H0, counterpart_z_array_event)
-                likelihood_array [hh] = simpson (px_z_H0*zprior_event*counterpart_pdf, counterpart_z_array_event)/H0**3
+                    px_z_H0 = self.likelihood_x_z_H0_single_event (event, H0, em_name, counterpart_z_array_event)
+                    likelihood_array [hh] = simpson (px_z_H0*zprior_event*counterpart_pdf, counterpart_z_array_event)/H0**3
 
-            likelihood_array /= simpson (likelihood_array, self.H0_array)
+                likelihood_array /= simpson (likelihood_array, self.H0_array)
 
-            likelihood_dictionary [event] = likelihood_array
+                likelihood_dictionary [f'{event}_{em_name}'] = likelihood_array
 
-            likelihood_df = pd.DataFrame(likelihood_dictionary)
+                likelihood_df = pd.DataFrame(likelihood_dictionary)
 
         return likelihood_df
             
